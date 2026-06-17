940c39d1ad5d778d544d69d637a72a342977b2f4
Mr.Fathan_KodingNext <fathan@kodingnext.com>
2026-06-17 15:16:28 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -6,6 +6,8 @@ import os
 
 st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
 
+st.title("SMART HOSPITAL NAVIGATOR")
+
 st.markdown("""
  <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
